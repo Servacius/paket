7e79f6157c2a7b9f75f3cb4fc5f0574e1b87d7ba
Arief Sibuea <sibuearief@gmail.com>
2021-06-21 13:28:15 +0700

    feat(petugas): send mail

---
+++ app/Mail/PaketEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PaketEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('penitipanpaket@gmail.com')
+            ->view('mail.paket')
+            ->with([
+                'nama' => $this->data['nama']
+            ]);
+    }
+}
---
+++ resources/views/mail/paket.blade.php
@@ -0,0 +1,3 @@
+<h3>Halo, {{ $nama }} !</h3>
+<p>Paket Anda telah sampai. Segera konfirmasi cara penerimaan paket.</p>
+<p>Terima kasih.</p>
